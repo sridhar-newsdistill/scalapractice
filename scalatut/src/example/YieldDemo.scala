976ab2ef3828c796edaf01ee6fae01010117c8ff
sridhar-newsdistill <sridhar.newsdistill@gmail.com>
2016-12-12 01:53:00 +0530

    yield demo

--- scalatut/src/example/YieldDemo.scala
+++ scalatut/src/example/YieldDemo.scala
@@ -3,7 +3,12 @@ package example
 object YieldDemo {
   def main(args: Array[String]): Unit = {
     var x = List(9, 7, 23, 5, 1, 5, 8);
-    for { p <- x if p != 2; if p < 9 }
+    var yieldVal = for { p <- x if p != 2; if p < 9 }
       yield p;
+
+    for (a <- yieldVal) {
+      println(a);
+    }
   }
+
 }
