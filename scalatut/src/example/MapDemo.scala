e67bf7542a947db0e1bfa7d8e88818afdd79b7c6
sridhar-newsdistill <sridhar.newsdistill@gmail.com>
2016-12-12 01:33:46 +0530

    scala for loop for range of values

--- scalatut/src/example/MapDemo.scala
+++ scalatut/src/example/MapDemo.scala
@@ -1,7 +1,19 @@
 package example
 
 object MapDemo {
-  def main(arg:String){
-    
+  def main(arg: Array[String]) {
+
+    var x: Int = 100;
+    for (a <- 1 to 100) {
+      println(a)
+    }
+
+  }
+  def max(a: Int, b: Int): Int = {
+    if (a > b) {
+      a
+    } else {
+      b
+    }
   }
 }
