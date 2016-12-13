8c67f47f7f583b67aeb6690b84fd4fd3d2f2fa2d
sridhar-newsdistill <sridhar.newsdistill@gmail.com>
2016-12-13 23:23:51 +0530

    list concat exaple

--- scalatut/src/example/ListDemo.scala
+++ scalatut/src/example/ListDemo.scala
@@ -1,7 +1,11 @@
 package example
 
 object ListDemo {
-  def main(arr:Array[String]):Unit={
-    val oneTwoThree=List(1,2,3);
+  def main(arr: Array[String]): Unit = {
+    val oneTwoThree = List(1, 2, 3);
+    val threeFour = List(3, 4);
+    val oneTwoThreeFour=oneTwoThree:::threeFour;
+    val newone = 4::oneTwoThreeFour;
+    println(oneTwoThreeFour)
   }
 }
