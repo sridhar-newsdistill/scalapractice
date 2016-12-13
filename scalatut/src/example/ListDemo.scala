809655ec88e0bf76a3977de35e7f9db150f7b043
sridhar-newsdistill <sridhar.newsdistill@gmail.com>
2016-12-13 23:51:03 +0530

    mutbale map included

--- scalatut/src/example/ListDemo.scala
+++ scalatut/src/example/ListDemo.scala
@@ -1,5 +1,6 @@
 package example
 import scala.collection.mutable.Set;
+import scala.collection.mutable.Map;
 object ListDemo {
   def main(arr: Array[String]): Unit = {
     val oneTwoThree = List(1, 2, 3);
@@ -14,6 +15,9 @@ object ListDemo {
 
     jetSet += "aribus";
     info += "hello";
-    println(info)
+    println(info);
+    val treasureMap=Map[Int,String]();
+    treasureMap+=(2->"sridhar");
+    treasureMap+=(3->"");
   }
 }
