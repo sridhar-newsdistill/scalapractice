7b3784ce0355afe0239946f013426a83aa8da392
sridhar-newsdistill <sridhar.newsdistill@gmail.com>
2016-12-13 23:42:12 +0530

    mutable set demo

--- scalatut/src/example/ListDemo.scala
+++ scalatut/src/example/ListDemo.scala
@@ -1,15 +1,19 @@
 package example
-
+import scala.collection.mutable.Set;
 object ListDemo {
   def main(arr: Array[String]): Unit = {
     val oneTwoThree = List(1, 2, 3);
     val threeFour = List(3, 4);
-    val oneTwoThreeFour=oneTwoThree:::threeFour;
-    val newone = 4::oneTwoThreeFour;
+    val oneTwoThreeFour = oneTwoThree ::: threeFour;
+    val newone = 4 :: oneTwoThreeFour;
     //val newval = 1::2::3:Nil;
-    List("Sridhar","newsDistill","hyderabad")
+    List("Sridhar", "newsDistill", "hyderabad")
     println(oneTwoThreeFour);
-    var jetSet = Set("charter","airbus");
-    jetSet +="aribus";
+    var jetSet = Set("charter", "airbus");
+    val info = Set("sridhar", "sri");
+
+    jetSet += "aribus";
+    info += "hello";
+    println(info)
   }
 }
