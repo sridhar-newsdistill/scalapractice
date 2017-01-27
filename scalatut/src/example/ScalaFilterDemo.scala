fb96ad4e9fce65ea21787967a98d6955fef6f6b8
sridhar-newsdistill <sridhar.newsdistill@gmail.com>
2017-01-28 00:52:18 +0530

    filter demo has beend added

---
+++ scalatut/src/example/ScalaFilterDemo.scala
@@ -0,0 +1,10 @@
+package example
+
+object ScalaFilterDemo {
+  def main(data:Array[String]){
+    
+  }
+  def filterDemo(delim:Int,dat:List[Int]):List[Int]={
+   return dat.filter(_<delim);
+  }
+}
